dd8f55bf7d22e7e6d1de2fd2ff600ec3960b9a27
stephen.chuang <stephen.chuang@twsit.com>
2025-12-02 11:25:02 +0800

    Add missing locale exports for language selector

--- stephen-taipei-react/src/i18n/locales/index.js
+++ stephen-taipei-react/src/i18n/locales/index.js
@@ -1,26 +1,48 @@
 import de from './de';
 import en from './en';
-import esMX from './es-MX';
 import es from './es';
+import esMX from './es-MX';
 import fil from './fil';
 import fr from './fr';
 import hi from './hi';
 import id from './id';
 import it from './it';
 import ja from './ja';
+import km from './km';
+import ko from './ko';
+import lo from './lo';
+import ms from './ms';
+import my from './my';
+import pt from './pt';
+import ptBR from './pt-BR';
+import th from './th';
+import vi from './vi';
+import zhCN from './zh-CN';
+import zhHK from './zh-HK';
 import zhTW from './zh-TW';
 
 export const translations = {
   de,
   en,
-  'es-MX': esMX,
   es,
+  'es-MX': esMX,
   fil,
   fr,
   hi,
   id,
   it,
   ja,
+  km,
+  ko,
+  lo,
+  ms,
+  my,
+  pt,
+  'pt-BR': ptBR,
+  th,
+  vi,
+  'zh-CN': zhCN,
+  'zh-HK': zhHK,
   'zh-TW': zhTW,
   zh: zhTW,
 };
